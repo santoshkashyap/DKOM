014b18778e82bb77fee23e39a90d85f3a6e11079
Santosh Kumar <santosh.kumar09@sap.com>
2016-10-20 09:59:44 +0530

    Initial Commit

---
+++ src/com/sap/dkom/servlet/AppServlet.java
@@ -0,0 +1,41 @@
+package com.sap.dkom.servlet;
+
+import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AppServlet
+ */
+@WebServlet("/")
+public class AppServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor.
+     */
+    public AppServlet() {
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Welcome to Continuos Integration and Continuos Deployment seesion on HCP!!!").append(" On "+ new Date());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
